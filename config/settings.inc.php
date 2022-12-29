76c3409d9406217a14b1cf20ca805f85ce20af16
leekayden <kayden@programmer.net>
2022-12-29 14:26:16 +0800

    update

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,16 +1,16 @@
 <?php
 define('_DB_SERVER_', 'localhost');
-define('_DB_NAME_', 'coliving');
+define('_DB_NAME_', 'eden_co');
 define('_DB_USER_', 'coliving');
 define('_DB_PASSWD_', 'tDi954t?8');
-define('_DB_PREFIX_', 'eden_');
+define('_DB_PREFIX_', 'co_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
 define('_PS_CACHE_ENABLED_', '0');
-define('_COOKIE_KEY_', 'ViiZX9ocjRc6htJuOOBUvV1Z8WmhKit1vqMXhBA1GgE7OUaxMEXvmgiC');
-define('_COOKIE_IV_', 'qvER9VMU');
-define('_NEW_COOKIE_KEY_', 'def00000ec172bf201f73702c729f62fd8655efcc30dedea66811f98824f9cacbfd34e23c76e9d6eea3eacd93e6581f3779b742c4a08e26775ae36402bc5bbc872e29c12');
-define('_PS_CREATION_DATE_', '2022-12-27');
+define('_COOKIE_KEY_', 'uavZFdMZI9eksGmiVddGuVirEcSWrXhjpbyRa8NUUBN64xoShrrYbjw5');
+define('_COOKIE_IV_', '8TBUKjmf');
+define('_NEW_COOKIE_KEY_', 'def00000c22bb503eba81fce9d0d50f8fae54e6310f426dd7f04c70714be4560f5cef4f325eba4a76d3395b79c5633adbd6214f0fb0539796ccc37f981a536dc58f4b479');
+define('_PS_CREATION_DATE_', '2022-12-28');
 if (!defined('_PS_VERSION_'))
 	define('_PS_VERSION_', '1.6.1.23');
 define('_QLOAPPS_VERSION_', '1.5.2.0');
